d5a501b367e30282e8e086b1f0718b1f4ebda3dc
Kübra Özdemir <kbraozdemir18@gmail.com>
2026-04-21 21:52:53 +0300

    fixed: access error

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-const PORT = process.env.PORT || 3000;
 
 const { Pool } = require('pg');
 
